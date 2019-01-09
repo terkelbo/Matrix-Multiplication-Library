e7743d527be4eb1cb588c20e9d62a0c8b8791798
s144315 <petervistar@hotmail.com>
2019-01-09 13:18:09 +0100

    Updated infrastructure

--- src/matmult_blk.c
+++ src/matmult_blk.c
@@ -6,23 +6,13 @@ matmult_blk(int m, int n, int k, double **A, double **B, double **C, int bs) {
     int i, j, l;
 	int blk_i, blk_j, blk_l;
 	
-	for(blk_i = 0; blk_i < m; blk_i += bs){ 
-		// start blk_i
-		for(i=blk_i;i < MIN(blk_i + bs, m); i++){ 
-			//start loop in blk_i
+	for(blk_i = 0; blk_i < m; blk_i += bs){
+		for(blk_l = 0; blk_l < k; blk_l += bs){
 			for(blk_j = 0; blk_j < n; blk_j +=bs){
-				for(j=blk_j;j < MIN(blk_j+bs,n); j++){ //START TO FILL THE BLOCK WITH ZEROS
-					C[i][j] = 0;
-				}
-			}
-			for(blk_l = 0; blk_l < k; blk_l += bs){
-				//start blk_l
-				for(l=blk_l;l < MIN(blk_l + bs, k);l++){
-					//start loop in blk_l
-					for(blk_j = 0; blk_j < n; blk_j +=bs){
-						//start blk_j
+				for(i=blk_i;i < MIN(blk_i + bs, m); i++){
+					for(l=blk_l;l < MIN(blk_l + bs, k);l++){
 						for(j=blk_j;j < MIN(blk_j + bs, n); j++){
-							//start loop in blk_j
+							if(l == 0){C[i][j] = 0;}
 							C[i][j] += A[i][l] * B[l][j];
 						}
 					}
