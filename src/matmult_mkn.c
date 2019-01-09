de0c30a3bcad4947a09da5b8f15830c55e6c583c
terkelbo <terkel_olsen@hotmail.com>
2019-01-09 21:19:16 +0100

    Add suncc results

--- src/matmult_mkn.c
+++ src/matmult_mkn.c
@@ -1,5 +1,5 @@
 void
-matmult_mkn(int m, int n, int k, double **A, double **B, double **C) {
+matmult_mkn(int m, int n, int k, double **restrict A, double **restrict B, double **restrict C) {
 
     int i, j, l;
     double x;
